7ebad520866c5125d915a4b18e2fadf02826a856
DenysSkobalo <denysskobalodev@gmail.com>
2024-06-09 00:38:00 +0200

    refactor: Removed the username field from the user model

--- internal/database/repositories/user_repository.go
+++ internal/database/repositories/user_repository.go
@@ -23,8 +23,8 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 }
 
 func (r *UserRepository) CreateUser(user *models.User) error {
-	query := "INSERT INTO users (username, email, password_hash, first_name, last_name, created_at) VALUES ($1, $2, $3, $4, $5, $6)"
-	_, err := r.DB.Exec(query, user.Username, user.Email, user.Password, user.FirstName, user.LastName, user.CreatedAt)
+	query := "INSERT INTO users (email, password_hash, first_name, last_name, created_at) VALUES ($1, $2, $3, $4, $5)"
+	_, err := r.DB.Exec(query, user.Email, user.Password, user.FirstName, user.LastName, user.CreatedAt)
 	if err != nil {
 		return fmt.Errorf("failed to create user: %v", err)
 	}
@@ -32,7 +32,7 @@ func (r *UserRepository) CreateUser(user *models.User) error {
 }
 
 func (r *UserRepository) GetAllUsers() ([]models.User, error) {
-	rows, err := r.DB.Query("SELECT id, username, email, password_hash, first_name, last_name, created_at FROM users")
+	rows, err := r.DB.Query("SELECT id, email, password_hash, first_name, last_name, created_at FROM users")
 	if err != nil {
 		return nil, err
 	}
@@ -41,7 +41,7 @@ func (r *UserRepository) GetAllUsers() ([]models.User, error) {
 	var users []models.User
 	for rows.Next() {
 		var user models.User
-		if err := rows.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.CreatedAt); err != nil {
+		if err := rows.Scan(&user.ID, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.CreatedAt); err != nil {
 			return nil, err
 		}
 		users = append(users, user)
--- internal/models/user.go
+++ internal/models/user.go
@@ -4,7 +4,6 @@ import "time"
 
 type User struct {
 	ID        int       `json:"id"`
-	Username  string    `json:"username"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	FirstName string    `json:"first_name"`
